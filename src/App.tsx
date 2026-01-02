7c9cf78bbb80f52c1a29bd9e346128b41c8d4ed3
Drexler Reyes <2xe2ipi@gmail.com>
2026-01-03 02:02:44 +0800

    compiled in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
 // src/App.tsx
+import { Header } from "./components/Header";
+import { OmniInput } from "./components/OmniInput";
+import { AppLayout } from "./layouts/AppLayout";
+
 function App() {
   return (
-    <div className="flex min-h-screen items-center justify-center">
-      <h1 className="text-3xl font-bold text-gray-800">
-        LifeOS Initialization Complete
-      </h1>
-    </div>
-  )
+    <AppLayout>
+      <Header />
+      <OmniInput />
+      
+      {/* Placeholder for Timeline (we will build this next) */}
+      <div className="flex-1 bg-gray-50 p-4 text-center text-gray-400 text-sm">
+        Timeline will load here...
+      </div>
+    </AppLayout>
+  );
 }
 
-export default App
+export default App;
